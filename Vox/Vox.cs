686e04b22f85fcd17d5030f8dfa1cd19a5f2920f
bradley <bradley@bradleymcgill.co.uk>
2023-12-28 18:32:43 +0000

    Can detect a key press to exit the screen

--- Vox/Vox.cs
+++ Vox/Vox.cs
@@ -7,4 +7,13 @@ class Vox: GameWindow{
     public Vox(int width, int height, string title) : 
     base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
     { }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape)){
+            Close();
+        }
+    }
 }
