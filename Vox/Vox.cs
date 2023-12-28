72904fcf6a7c2e76e90c0ec10a831585439c5448
bradley <bradley@bradleymcgill.co.uk>
2023-12-28 20:11:49 +0000

    Add colour into the program and can now diff via vertex

--- Vox/Vox.cs
+++ Vox/Vox.cs
@@ -8,15 +8,15 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 class Vox : GameWindow
 {
     float[] vertices = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f,   // top left
     };
 
     uint[] indices = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
+    0, 2, 6,   // first triangle
+    2, 4, 6    // second triangle
     };
 
     int VertexBufferObject;
@@ -53,6 +53,8 @@ class Vox : GameWindow
         GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
         GL.EnableVertexAttribArray(0);
 
+        GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+        GL.EnableVertexAttribArray(1);
 
         ElementBufferObject = GL.GenBuffer();
         GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
