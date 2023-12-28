addd898e541ebf716c82eea5ebe670de3b5b2ab3
bradley <bradley@bradleymcgill.co.uk>
2023-12-28 18:39:10 +0000

    Add onLoad, onRender and onResize methods

--- Vox/Vox.cs
+++ Vox/Vox.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -8,6 +9,23 @@ class Vox: GameWindow{
     base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
     { }
 
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(Color.AntiqueWhite);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        // stuff
+
+        SwapBuffers(); //OpenGL paints to a screen in the background then swaps with the one on screen when rendering to reduce tearing
+    }
+
     protected override void OnUpdateFrame(FrameEventArgs args)
     {
         base.OnUpdateFrame(args);
@@ -15,5 +33,12 @@ class Vox: GameWindow{
         if (KeyboardState.IsKeyDown(Keys.Escape)){
             Close();
         }
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
     }
 }
